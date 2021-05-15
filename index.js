32aa3ee566f5a7c0f5f490a2833e11ade15fe006
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-15 21:05:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 document.querySelector('#main').remove();
 
 let element = document.createElement('h1')
+element.
 document.body.appendChild(element)
+
+let newHeader = document.querySelector('#victory');
